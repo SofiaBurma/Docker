b6f3a9d90e4ef9bbe6d927d5b4504094acd8b069
SofiaBurma <sofiburma-ipt28@lll.kpi.ua>
2025-05-30 20:08:38 +0300

    test app

--- test_app/test.py
+++ test_app/test.py
@@ -1,14 +1,21 @@
-import pytest
-from flask import Flask
-from app.application import app as flask_app 
+# import pytest
+# from flask import Flask
+# from app.application import app as flask_app 
 
-@pytest.fixture
-def client():
-    flask_app.config['TESTING'] = True
-    with flask_app.test_client() as client:
-        yield client
+# @pytest.fixture
+# def client():
+#     flask_app.config['TESTING'] = True
+#     with flask_app.test_client() as client:
+#         yield client
 
-def test_home_route(client):
-    response = client.get('/')
+# def test_home_route(client):
+#     response = client.get('/')
+#     assert response.status_code == 200
+#     assert response.json == {"message": "Hello"}
+
+import requests
+
+def test_home_route():
+    response = requests.get("http://localhost:5000/")
     assert response.status_code == 200
-    assert response.json == {"message": "Hello"}
+    assert response.json() == {"message": "Hello"}
